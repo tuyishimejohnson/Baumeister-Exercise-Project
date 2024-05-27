24f31ddad0f5d732c613cdc9349c114ce5ba619e
edine-noella <edinenoella@gmail.com>
2024-05-27 16:53:36 +0200

    chore: added custom fonts/color & configured prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],        
+      },
+      colors: {
+         green: "#00672E",
+         yellow: "#FFE800",
+         red: "#E31C24",
+         blue: "#0F2CCE",
+         pink: "#FF93C8",
+         black: "#000000",
+
+
+      }
+    },
   },
   plugins: [],
 }
